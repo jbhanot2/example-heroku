f1e4e4717644b5fe6280c5580c9b585540c5e7ce
jatin bhanot <jatinbhanot@nicobar.com>
2020-05-23 23:42:59 +0530

    changes

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+ });
+ 
 app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
-});
+    console.log('Server is up!');
+ });
